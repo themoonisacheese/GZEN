8e77c79da4c3c04258f1d37fc14a47a58904a6cd
olivier <oliviersenn6@gmail.com>
2019-02-04 22:42:31 +0100

    add instructions

--- wall.py
+++ wall.py
@@ -4,5 +4,5 @@ from gridAlignedObject import GridAlignedObject
 
 class Wall(GridAlignedObject):
     def __init__(self, gridPosition):
+        #TODO:randomly assign cracked wall texture
         GridAlignedObject.__init__(self, gridPosition, aggregateAnim('sprites/murs/', 'mur simple'), 0.001)
-        #do texture scaling stuff here.
