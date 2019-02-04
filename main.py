efa85df2c354c8930bbc5baa0204222bd712e2bb
olivier <oliviersenn6@gmail.com>
2019-02-04 18:03:22 +0100

    automatic animation processing

--- main.py
+++ main.py
@@ -2,19 +2,22 @@ import sys, pygame
 from displayableElement import DisplayableElement
 from screenmanager import ScreenManager
 from array import array
+from animationAggregator import aggregateAnim
 pygame.init()
 size = width, height = 1280, 720
 SM = ScreenManager(size)
 disElems = []
-disElems.append(DisplayableElement('sprites/bat/bat_Animation 1_0.png'))
-disElems.append(DisplayableElement('sprites/bat/bat_Animation 1_5.png'))
+disElems.append(DisplayableElement(aggregateAnim('sprites/bat/', 'bat_Animation'), 0.1))
 clock = pygame.time.Clock()
+
 while 1:
-    clocktick = clock.tick(60) # on peut multiplier toutes les vitesses par ca pour les adapater au framerate
+    clocktick = clock.tick(30) # on peut multiplier toutes les vitesses par ca pour les adapater au framerate
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         #TODO:process keypresses etc
-    dep = 2,2
-    disElems[1].move(dep)
+
+
+    for elem in disElems:
+        elem.animate(clocktick)
     SM.displayElements(disElems)
