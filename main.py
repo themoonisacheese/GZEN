9d66f166857334ce8c0e922a0375c9029e52fc97
Koreos <salisalut@laposte.net>
2019-02-05 11:33:54 +0100

    Timer stuff

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ pygame.init()
 size = width, height = 1280, 576
 SM = ScreenManager(size)
 clock = pygame.time.Clock()
+time = 0
 pygame.key.set_repeat(1, 500)
-
 
 gameObjects = [Wall((0, 0)), Wall((4, 5)), Wall((2, 5)), Wall((3, 5)), Wall((4, 5)), Wall((4, 6)), Wall((4, 7)),
                Wall((4, 8)), Wall((4, 2)), Wall((4, 3))]
@@ -23,6 +23,13 @@ for obj in gameObjects:
 
 while 1:
     clocktick = clock.tick(30)  # on peut multiplier toutes les vitesses par ca pour les adapater au framerate
+    # Timer part
+    # Start this when someone clicks on play or whatever
+    seconds = clocktick/1000
+    time += seconds  # while time < 180...
+    font = pygame.font.Font(None, 18)
+    timeleft = font.render(str(time), 1, (0, 0, 255))  # how to display ??
+    # End timer part
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
