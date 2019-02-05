67361d614b156340a1fc9fd2453866771829d19f
olivier <oliviersenn6@gmail.com>
2019-02-05 23:39:19 +0100

    added temporary animations

--- displayableElement.py
+++ displayableElement.py
@@ -1,5 +1,6 @@
 class DisplayableElement:
     display = True
+    isInTempAnim = False
     __timeSinceLastAnimation = 0
     __currentAnimationFrameIndex = 0
 
@@ -26,6 +27,11 @@ class DisplayableElement:
                 self.__timeSinceLastAnimation = 0
                 self.__currentAnimationFrameIndex += 1
                 if self.__currentAnimationFrameIndex >= len(self.animationFrames):
+                    if self.isInTempAnim:
+                        self.animationFrames = self.__originalAnimation
+                        self.animationFrameRate = self.__originalFR
+                        self.isInTempAnim = False
+                        self.__tempAnimCallBack(self)
                     self.__currentAnimationFrameIndex = 0
                 if self.currentTexture is not self.animationFrames[self.__currentAnimationFrameIndex]:
                     self.currentTexture = self.animationFrames[self.__currentAnimationFrameIndex]
@@ -36,6 +42,14 @@ class DisplayableElement:
         self.__currentAnimationFrameIndex = 0
         self.currentTexture = self.animationFrames[0]
         self.rect = self.currentTexture.get_rect()
+
+#this changes the animation for one loop. after the loop, the original animation will be restored and callback(self) will be called
+    def changeAnimationTemp(self, tempAnimation, animationFrameRate, callback): #callback must be a function that takes 1 argument: the object that called it.
+        self.__originalAnimation = self.animationFrames
+        self.__originalFR = self.animationFrameRate
+        self.__tempAnimCallBack = callback
+        self.isInTempAnim = True
+        self.changeAnimation(tempAnimation, animationFrameRate)
 
     def update(self, ticktime, objlist):
         pass
