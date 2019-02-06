566e066f21b774c134edffa92fc6f734cc7687a3
Koreos <salisalut@laposte.net>
2019-02-06 19:33:46 +0100

    Merge branch 'master' of https://github.com/themoonisacheese/GZEN
    # Conflicts:
    #	inputManager.py
    #	main.py

--- inputManager.py
+++ inputManager.py
@@ -14,7 +14,7 @@ def processInputs(event, char):  # event.type is guaranteed to be pygame.KEYDOWN
         if event.key in upkeys:
             # print(char.movementVector)
             # print("up")
-            char.addToVec(0, -600)
+            char.addToVec(0, -300)
             # char.movementVector = char.movementVector[0], char.movementVector[1] - 628 * (clocktick/1000)
             # print(char.movementVector)
         elif event.key in rightkeys:
@@ -46,38 +46,3 @@ def processInputs(event, char):  # event.type is guaranteed to be pygame.KEYDOWN
             sys.exit()
     elif event.type == pygame.KEYUP:
         print("cc")
-
-
-    if event.key in upkeys:
-        # print(char.movementVector)
-        # print("up")
-        char.addToVec(0, -300)
-        # char.movementVector = char.movementVector[0], char.movementVector[1] - 628 * (clocktick/1000)
-        # print(char.movementVector)
-    elif event.key in rightkeys:
-        # print(char.movementVector)
-        # print("right")
-        if char.movementVector[0] >= 0:
-            char.movementVector = char.movementVector[0] - 64, char.movementVector[1]
-            # x = 64
-        elif 0 > char.movementVector[0] > -208:
-            char.movementVector = char.movementVector[0] - 32, char.movementVector[1]
-            # x = 32
-        # print(char.movementVector)
-        # print('Val ajoutee : ' + str(x))
-    elif event.key in leftkeys:
-        # print(char.movementVector)
-        # print("left")
-        if char.movementVector[0] <= 0:
-            char.movementVector = char.movementVector[0] + 64, char.movementVector[1]
-            # x = 64
-        elif 0 < char.movementVector[0] < 208:
-            char.movementVector = char.movementVector[0] + 32, char.movementVector[1]
-            # x = 32
-        # print(char.movementVector)
-        # print('Val ajoutée : ' + str(x))
-    elif event.key == pygame.K_f:
-        print("cc")  # attack
-    elif event.key == pygame.K_ESCAPE:
-        print("Quitting game...")
-        sys.exit()
