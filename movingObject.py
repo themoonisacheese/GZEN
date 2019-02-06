905af3099863dba4842e26e7cd48dcef5ccdb275
Koreos <salisalut@laposte.net>
2019-02-06 11:18:03 +0100

    aaa
    a

--- movingObject.py
+++ movingObject.py
@@ -11,11 +11,10 @@ class MovingObject(CollisionObject):
         realVector = self.movementVector[0] * (ticktime/1000.0), self.movementVector[1] * (ticktime/1000.0)
         self.__additiveVector = self.__additiveVector[0] + realVector[0], self.__additiveVector[1] + realVector[1]
         self.move(self.__additiveVector)
-        if self.__additiveVector[0] >=1: #this is to make sure that we can still move even if the framerate is high. having a high framerate induces sub-pixel moves that do not work.
+        if self.__additiveVector[0] >=1:  # this is to make sure that we can still move even if the framerate is high. having a high framerate induces sub-pixel moves that do not work.
             self.__additiveVector = 0, self.__additiveVector[1]
         if self.__additiveVector[1] >=1:
             self.__additiveVector = self.__additiveVector[0], 0
-
 
     def changeVec(self, newVec):
         self.movementVector = newVec
