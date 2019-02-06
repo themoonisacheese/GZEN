5a73d16de994307dec5398dde4d4a3ea65bbe269
Koreos <salisalut@laposte.net>
2019-02-06 15:20:41 +0100

    oops left and right is hard

--- inputManager.py
+++ inputManager.py
@@ -3,8 +3,8 @@ import pygame
 import sys
 
 upkeys = [pygame.K_UP, pygame.K_z, pygame.K_w, pygame.K_SPACE]
-leftkeys = [pygame.K_LEFT, pygame.K_d]
-rightkeys = [pygame.K_RIGHT, pygame.K_q, pygame.K_a]
+leftkeys = [pygame.K_RIGHT, pygame.K_d]
+rightkeys = [pygame.K_LEFT, pygame.K_q, pygame.K_a]
 
 
 def processInputs(clocktick, event, char):  # event.type is guaranteed to be pygame.KEYDOWN
