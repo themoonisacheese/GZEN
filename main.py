7faf34feffa52aa81f20ee77b76dde0d5288f793
RoxaneD <roxane.deflandre2@gmail.com>
2019-02-07 16:18:08 +0100

    score

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ size = width, height = 1024, 576
 SM = ScreenManager(size)
 clock = pygame.time.Clock()
 time = 180
-roomNumber = 3
+roomNumber = 1
 floorNumber = 1
 pygame.key.set_repeat(1, 200)
 
@@ -31,12 +31,14 @@ for x in range(16):
 gameObjects.append(Room('design niveaux/lvl1.png', roomNumber, floorNumber))
 gameObjects.append(Player())
 gameObjects.append(DisplayableElement(aggregateAnim('sprites/environment', 'rectangle')))
-gameObjects.append(TextElement('texte', 'Calibri', 40, (189, 18, 18)))
+gameObjects.append(DisplayableElement(aggregateAnim('sprites/environment', 'rectangle')))
+gameObjects.append(TextElement('texte', 'Calibri', 35, (189, 18, 18)))
+gameObjects.append(TextElement('texte', 'Calibri', 35, (189, 18, 18)))
 # THE ORDER OF GAMEOBJECTS IN THE LIST IS REALLY IMPORTANT: IT DICTATES DRAWING ORDER. DO NOT MESS WITH IT
 
 for obj in gameObjects:
     obj.display = True
-gameObjects[-4].show(True)
+gameObjects[-6].show(True)
 
 while 1:
     clocktick = clock.tick(60)  # on peut multiplier toutes les vitesses par ca pour les adapater au framerate
@@ -45,34 +47,37 @@ while 1:
     seconds = clocktick/1000.0
     time -= seconds  # while time < 180...
     timeleft = str(int(time)) + "s left!"
-    gameObjects[-1].setText(timeleft)  # dirty adressing atm
-    gameObjects[-1].moveto((98, 28))
+    gameObjects[-2].setText(timeleft)  # dirty adressing atm
+    gameObjects[-2].moveto((98, 28))
     # End timer part
     # Score part
-    # print("oui")
+    score = str(gameObjects[-5].score)
+    gameObjects[-1].setText(score)
+    gameObjects[-1].moveto((920, 25))
+    gameObjects[-3].moveto((920, 32))
     # End score part
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         if event.type == pygame.KEYDOWN or event.type == pygame.KEYUP:
-            processInputs(event, gameObjects[-3])
+            processInputs(event, gameObjects[-5])
 
     #room handling
-    if gameObjects[-3].rect.centerx > 1024:
+    if gameObjects[-5].rect.centerx > 1024:
         roomNumber += 1
         if roomNumber >= 8:
             roomNumber = 0
-        gameObjects[-4] = Room('design niveaux/lvl1.png', roomNumber, floorNumber)#FIXME
-        gameObjects[-4].show(True)
-        gameObjects[-3].rect.centerx = 0
+        gameObjects[-6] = Room('design niveaux/lvl1.png', roomNumber, floorNumber)#FIXME
+        gameObjects[-6].show(True)
+        gameObjects[-5].rect.centerx = 0
 
-    if gameObjects[-3].rect.centerx < 0:
+    if gameObjects[-5].rect.centerx < 0:
         roomNumber -= 1
         if roomNumber <= -1:
             roomNumber = 7
-        gameObjects[-4] = Room('design niveaux/lvl1.png', roomNumber, floorNumber)#FIXME
-        gameObjects[-4].show(True)
-        gameObjects[-3].rect.centerx = 1024
+        gameObjects[-6] = Room('design niveaux/lvl1.png', roomNumber, floorNumber)#FIXME
+        gameObjects[-6].show(True)
+        gameObjects[-5].rect.centerx = 1024
 
     for obj in gameObjects:
         obj.update(clocktick, gameObjects)
