e9363ddcef0a91fe4ff205802340ce7931148571
thecanarddefer <climarto@gmail.com>
2019-02-08 00:40:05 +0100

    spring

--- main.py
+++ main.py
@@ -19,13 +19,14 @@ size = width, height = 1024, 576
 SM = ScreenManager(size)
 clock = pygame.time.Clock()
 time = 180
+white=False
 #<<<<<<< Updated upstream
 roomNumber = 7
 floorNumber = 3
 #=======
 #roomNumber = 0
 #floorNumber = 1
-white=False
+
 #>>>>>>> Stashed changes
 pygame.key.set_repeat(1, 200)
 # Music
